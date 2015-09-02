cdcb53673f226688467f73b2d1fe18b46c6cad2b
itzeltlelocl <itzel.tlelocl@udlap.mx>
2015-09-02 17:03:34 -0500

    Update sherlock.js

--- sherlock.js
+++ sherlock.js
@@ -21,4 +21,26 @@ _INPUT = "2\n3 9\n17 24";
 1
 */
 
-//----- Start your code here -------
+package tarea2_id150154;
+import java.io.*;
+
+public class Tarea2_ID150154 {
+
+    public static void main(String[] args) throws IOException {
+        StringBuffer outputbuffer = new StringBuffer();
+        BufferedReader inputbuffer = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Input");
+        for(byte T = Byte.parseByte(inputbuffer.readLine()); T >= 1 && T <= 100; --T){
+            String[] input = inputbuffer.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+            
+            int Output = (int)(Math.floor(Math.sqrt(B)) - Math.ceil(Math.sqrt(A)) + 1);
+            outputbuffer.append(Output).append("\n");   
+        }
+        System.out.println("Output");
+        System.out.print(outputbuffer);
+       }
+    
+}
